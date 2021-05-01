b2ea9722a66e1ece9e14ebe3cd6867c6a1043e8c
moritz <theincxption@gmail.com>
2021-05-01 23:50:35 +0200

    :lipstick: Create color style guide

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const whiteBright = "#FEFEFE"
+export const whiteDark = "#EFEFEF"
+
+export const blueBright = "#6DD5ED"
+export const blueDark = "#288DB8"
+
+const obj = {
+   whiteBright,
+   whiteDark,
+   blueBright,
+   blueDark
+}
+
+export default obj
