90df71e79b178454590f918f7d84414519d29ffd
moritz <theincxption@gmail.com>
2021-05-01 23:54:34 +0200

    :art: Reformat index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import App from "@components/App"
 import "./styles/index.css"
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.body
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   document.body
 )
