fa6e4ecdd6a50c4bcf7a645d567a5bf5c01d3410
moritz <theincxption@gmail.com>
2021-05-01 23:54:20 +0200

    :bento: Add thumbnail for spotify-composer project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,38 @@
-import React from "react"
+import React, { useRef, useState } from "react"
+import AboutSlide from "@components/slide/about/AboutSlide"
+import LandingSlide from "./slide/landing/LandingSlide"
+import ProjectsSlide from "@components/slide/projects/ProjectsSlide"
+
+const slides = [LandingSlide, AboutSlide, ProjectsSlide]
+
+export const NavigationContext = React.createContext<{ navigateSlide(amount: number): void } | undefined>(undefined)
 
 const App: React.FC = () => {
-    return (
-        <div>
+   const [slideIndex, setSlideIndex] = useState(0)
+   const lastScroll = useRef(0)
 
-        </div>
-    )
+   const onWheel: React.WheelEventHandler<HTMLDivElement> = (event) => {
+      if (Date.now() - lastScroll.current < 300) {
+         return
+      }
+
+      navigateSlide(event.deltaY > 0 ? 1 : -1)
+   }
+
+   function navigateSlide(amount: number) {
+      setSlideIndex((prev) => Math.max(0, Math.min(prev + amount, slides.length - 1)))
+      lastScroll.current = Date.now()
+   }
+
+   return (
+      <NavigationContext.Provider value={{ navigateSlide }}>
+         <div onWheel={onWheel}>
+            {slides.map((SlideComponent, index) => (
+               <SlideComponent state={index <= slideIndex ? "current" : index === slideIndex + 1 ? "next" : "hidden"} />
+            ))}
+         </div>
+      </NavigationContext.Provider>
+   )
 }
 
 export default App
